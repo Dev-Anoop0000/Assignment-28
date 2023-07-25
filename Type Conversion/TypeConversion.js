d9da8275f28b2309bd1cc0eaab2e798a7196ba80
Anoop Kumar Gupta <116798455+Dev-Anoop0000@users.noreply.github.com>
2023-07-25 10:41:21 +0530

    Add files via upload

---
+++ Type Conversion/TypeConversion.js
@@ -0,0 +1,18 @@
+// Write a function called convertToNumber that takes a string as an argument and returns the equivalent number. If the string cannot be converted to a number, the function should return the string "Invalid number". Use error handling in javascript to achieve this output.
+
+console.log("Solution....");
+
+function convertToNumber(str) {
+    try {
+        const num = Number(str);
+        if (Number.isNaN(num)) {
+            throw new Error("Invalid number");
+        }
+        return num;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+console.log(convertToNumber("123456"));
+console.log(convertToNumber("Anoop"));
